d55a4f30d4440b3b9046ced80b5f39e2e5aa9d4e
nosix <i.nosix@gmail.com>
2016-12-06 00:03:50 +0900

    テスト実行でHTMLを生成

--- src/test/kotlin/org/anyspirit/snippet/thymeleaf/ThymeleafSnippetJaApplicationTests.kt
+++ src/test/kotlin/org/anyspirit/snippet/thymeleaf/ThymeleafSnippetJaApplicationTests.kt
@@ -4,13 +4,20 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.web.client.RestTemplate
+import java.nio.file.*
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
 class ThymeleafSnippetJaApplicationTests {
 
 	@Test
-	fun contextLoads() {
+	fun generateHtml() {
+		RestTemplate().getForObject("http://localhost:8080/", String::class.java).run {
+			Files.newBufferedWriter(Paths.get("generated.html")).use {
+				it.write(this)
+			}
+		}
 	}
 
 }
